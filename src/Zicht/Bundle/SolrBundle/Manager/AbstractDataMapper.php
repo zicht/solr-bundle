13f216527a66870255fcf89b6f0a02998a0e5e43
Gerard van Helden <gerard@zicht.nl>
2015-10-23 16:52:48 +0000

    clone the datetime object before mutating the timezone
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtSolrBundle/trunk@71548 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/SolrBundle/Manager/AbstractDataMapper.php
+++ src/Zicht/Bundle/SolrBundle/Manager/AbstractDataMapper.php
@@ -32,8 +32,10 @@ abstract class AbstractDataMapper implements DataMapperInterface
         if (null === $dateTime) {
             return null;
         }
-        $dateTime->setTimezone(new \DateTimeZone('UTC'));
-        return $dateTime->format(self::DATE_FORMAT);
+        // force the timezone to be set to UTC, but DON'T mutate the object.
+        $cloned = clone $dateTime;
+        $cloned->setTimezone(new \DateTimeZone('UTC'));
+        return $cloned->format(self::DATE_FORMAT);
     }
 
     /**
